d5ee914e09e94dbda60e52899129599e1f8854fb
Dnyaneshwar Gorade <dngorade@cisco.com>
2020-01-22 13:42:33 +0530

    char-linked-list programming test ciena

--- vector.cpp
+++ vector.cpp
@@ -1,64 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
+#include <deque>
+#include <list>
+#include<algorithm>
 
 using namespace std;
 
-template <class T>
-void hello(T t) 
-{
-  cout << "data= " << t;
-}
-
-template <class T>
-class Array {
-  T data;
-
-  public:
-    void setData(T t) {
-      data = t;
-    }
-    void printData() {
-      cout << "\nData= " << data;
-    }
-};
-
 int main()
 { 
-    Array<int> a0;
-    a0.setData(500);
-    a0.printData();
+    deque<int> dq(5);
+    
+    cout << "Deque Example:" << endl;
+    for (int i = 0; i < 5; i++) {
+        dq[i] = i + 1;
+    }
+    dq.push_back(10);
+    dq.push_front(100);
+    for (int i = 0; i < dq.size(); i++) {
+        cout << dq[i] << " ";
+    }
+    
+    if (!dq.empty())
+      cout << "\nDeque is not empty, size = " << dq.size();
 
-    Array<double> a1;
-    a1.setData(500.32);
-    a1.printData();
+    cout << "\n\nVector Example:" << endl;
+    vector<int> vc(5);
 
-    cout << endl;
-    char ch;
-    fstream fin, fout;
-    fin.open("sample.txt", ios::in);
-    fout.open("sample-out.txt", ios::out);
-    while (!fin.eof()) {
-      fin.get(ch);
-      cout << ch;
-      fout.put(ch);
+    vc.resize(7);
+    for (int i = 0; i < vc.size(); i++) {
+        vc[i] = i + 1;
+    }
+    vc.push_back(6);
+    auto it = vc.begin()+2;
+    vc.insert(it, 2, 333);
+    vc.insert(vc.begin()+5, 555);
+    for (auto it = vc.begin(); it != vc.end(); it++) {
+        cout << *it << " ";
+    }
+    if (!vc.empty())
+      cout << "Vector is not empty, size = " << vc.size();
+
+    cout << "\n\nLists Example:" << endl;
+    list<int> lst;
+
+    for (int i = 0; i < 5; i++) {
+        lst.push_back(i+1);
     }
 
-    fout.close();
-    fout.open("sample-out.txt", ios::in);
-
-    while (!fout.eof()) {
-      fout >> noskipws >> ch;
-      cout << ch;
+    for (auto it = lst.begin(); it != lst.end(); it++) {
+       cout << *it << " "; 
     }
-    fin.close();
-    fout.close();
+    list<int>::iterator lst_it = find(lst.begin(), lst.end(), 4);
+    cout << "\nFound item: " << *lst_it;
 
-    cout << endl;
-    hello(12);
-    hello(12.23);
-
-    cout << endl;
+    cout << endl << endl;
     return 0;
 }
 
